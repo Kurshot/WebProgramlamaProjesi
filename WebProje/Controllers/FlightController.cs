f31fc9b5962001c935e472e7a33eebf5f3011e99
Eray <polat.eray.1977@gmail.com>
2023-12-23 19:42:36 +0300

    FlightController

--- WebProje/Controllers/FlightController.cs
+++ WebProje/Controllers/FlightController.cs
@@ -17,24 +17,90 @@ namespace WebProje.Controllers
                 Include(z=>z.Plane.PlaneType);
             return View(list.ToList());
         }
-        [HttpPost]
+        [HttpGet]
         public IActionResult Add()
         {
             ViewBag.departurePlaceId = new SelectList(o.Airports, "Id", "AirportName");
             ViewBag.arrivalPlaceId = new SelectList(o.Airports, "Id", "AirportName");
+            ViewBag.PlaneId = new SelectList(o.PlaneTypes, "Id", "modelName");
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Add(Flight f)
         {
             if (ModelState.IsValid)
             { 
-                o.Add(f);
+                o.Flights.Add(f);
                 o.SaveChanges();
                 TempData["msj"] = "Uçuş eklendi";
                 return RedirectToAction("List");
             }
             ViewBag.departurePlaceId = new SelectList(o.Airports, "Id", "AirportName");
             ViewBag.arrivalPlaceId = new SelectList(o.Airports, "Id", "AirportName");
+            ViewBag.PlaneId = new SelectList(o.PlaneTypes, "Id", "modelName");
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                TempData["hata"] = "Lütfen bir uçuş seçiniz";
+                return View();
+            }
+            var ticket = o.Flights.Include(x => x.Tickets).FirstOrDefault(y => y.Id == id);
+            if(ticket is null)
+            {
+                TempData["hata"] = "Böyle bir uçuş bulunamadi";
+                return View();
+            }
+            if (ticket.Tickets.Count>0)
+            {
+                TempData["hata"] = "Bu uçuşa ait biletler var önce biletleri siliniz";
+                return View();
+            }
+            o.Flights.Remove(ticket);
+            o.SaveChanges();
+            TempData["msj"] = "Uçuş silindi";
+            return RedirectToAction("List");
+        }
+        public IActionResult Edit(int? id)
+        {
+            ViewBag.departurePlaceId = new SelectList(o.Airports, "Id", "AirportName");
+            ViewBag.arrivalPlaceId = new SelectList(o.Airports, "Id", "AirportName");
+            ViewBag.PlaneId = new SelectList(o.PlaneTypes, "Id", "modelName");
+            if (id == null)
+            {
+                TempData["hata"] = "Lütfen bir uçuş seçiniz";
+                return View();
+            }
+            var ticket = o.Flights.Include(x => x.Tickets).FirstOrDefault(y => y.Id == id);
+            if (ticket is null)
+            {
+                TempData["hata"] = "Böyle bir uçuş bulunamadi";
+                return View();
+            }
+            return View(ticket);
+        }
+        [HttpPost]
+        public IActionResult Edit(int ?id,Flight f)
+        {
+            if (id != f.Id)
+            {
+                TempData["hata"] = "Böyle bir uçuş yok";
+                return View();
+            }
+            if (ModelState.IsValid)
+            {
+                o.Flights.Update(f);
+                o.SaveChanges();
+                TempData["msj"] = "Uçuş düzenlendi";
+                return RedirectToAction("List");
+            }
+            ViewBag.departurePlaceId = new SelectList(o.Airports, "Id", "AirportName");
+            ViewBag.arrivalPlaceId = new SelectList(o.Airports, "Id", "AirportName");
+            ViewBag.PlaneId = new SelectList(o.PlaneTypes, "Id", "modelName");
+            TempData["hata"] = "Uçuş güncelleme başarısız";
             return View();
         }
     }
--- WebProje/Models/Flight.cs
+++ WebProje/Models/Flight.cs
@@ -12,15 +12,15 @@ namespace WebProje.Models
         public DateTime arrivalTime { get; set; }
         [Display(Name = "Uçak Türü")]
         public int PlaneId { get; set; }
-        public Plane Plane { get; set; }
+        public Plane? Plane { get; set; }
         [Display(Name = "Bilet Fiyatı")]
         public float price { get; set; }
         [Display(Name = "Kalkış Yeri")]
         public int? departurePlaceId { get; set; }
-        public Airport departurePlace { get; set; }
+        public Airport? departurePlace { get; set; }
         [Display(Name = "Varış Yeri")]
         public int? arrivalPlaceId { get; set; }
-        public Airport arrivalPlace { get; set; }
+        public Airport? arrivalPlace { get; set; }
         public ICollection<Ticket>? Tickets { get; set; }
     }
 }
