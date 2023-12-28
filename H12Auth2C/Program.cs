2f7118589866aec5734a439c44a641b65cc987f6
kurshot <sonkurahmetkursat@gmail.com>
2023-12-28 20:17:21 +0300

    update login page

--- H12Auth2C/Program.cs
+++ H12Auth2C/Program.cs
@@ -14,7 +14,10 @@ namespace H12Auth2C
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+                        //builder.Services.AddDefaultIdentity<UserDetails>(options => options.SignIn.RequireConfirmedAccount = true)
+                //.AddEntityFrameworkStores<ApplicationDbContext>();
+           // builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
           //  builder.Services.AddDefaultIdentity<UserDetails>(options => options.SignIn.RequireConfirmedAccount = true)
            //     .AddEntityFrameworkStores<ApplicationDbContext>();
